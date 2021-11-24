6ea835ec9d35c657f53d11e597a3cd98609e20d9
archana vinayagam <saiarchana.93@gmail.com>
2021-11-24 13:57:19 +0530

    user can able to select only two cards at a time

--- exercise/scripts/main.js
+++ exercise/scripts/main.js
@@ -6,11 +6,12 @@ window.addEventListener('DOMContentLoaded', function() {
 
   cards.forEach(function(card) {
     card.addEventListener('click', function() {
+      selectedCards.push(card);
       // If the card has already been matched, ignore it.
       if (card.classList.contains('is-matched')) {
         return;
       }
-
+      console.log('only one card is selected');
       // If we haven't selected 2 cards yet, add the current card to the
       // collection of selected cards and apply the correct CSS class.
       if (selectedCards.length < 2) {
@@ -20,8 +21,11 @@ window.addEventListener('DOMContentLoaded', function() {
       // If we have selected two cards, see if they match.
       if (selectedCards.length === 2) {
         var card1 = selectedCards[0];
-        var card2 = selectedCards[0];
+        var card2 = selectedCards[1];
 
+        card1.classList.add('is-selected');
+        card2.classList.add('is-selected');
+        console.log('two cards has been selected');
         // If the cards match, add them to the collection of matched cards and
         // apply the correct CSS class.
         if (card1.innerText === card2.innerText) {
